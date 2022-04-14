f7faaca985d7c0e413f2d84fd13e121e1973b3c6
PabloLandro <73550306+PabloLandro@users.noreply.github.com>
2022-04-15 01:40:55 +0200

    pls help

--- Ejercicios 2020/Apartado A/Main.java
+++ Ejercicios 2020/Apartado A/Main.java
@@ -1,72 +1,125 @@
 import java.io.*;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Main {
 
+    static int[] lines;
+    static int[] cases;
+    static Integer[] indexes;
+    static int size;
+
     public static void main(String[] args) {
+
         // read lines of file as array of ints
-        int[] lines = readInput();
+        crearArchivo();
+        readInput();
 
-        // iterate over lines
-        for (int line : lines) {
-            System.out.println(poirot(line) +"");
+        orderArray();
 
+        poirot();
+        crash();
+        System.exit(0);
+        printResults();
+
+        System.exit(0);
+    }
+
+    static void crearArchivo() {
+        // create file
+        File file = new File("output.txt");
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // tomar Integer max value -1
+        // write to file
+        try {
+            FileWriter fw = new FileWriter(file.getAbsoluteFile());
+            BufferedWriter bw = new BufferedWriter(fw);
+            //guardar en variable integer max value -1
+            bw.write(100+ "\n");
+            //escribir tantas ints aleatorias en el archivo como integer max value -1
+            for (int i = 500000; i > 499900; i--) {
+                bw.write(i + "\n");
+            }
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    static void orderArray () {
+        cases = new int[size];
+        indexes = new Integer[size];
+        for (int i = 0; i < size; i++) {
+            indexes[i] = new Integer(i);
+        }
+        Comparator<Integer> comp = new Comparator<Integer>() {
+            @Override
+            public int compare(final Integer o1, final Integer o2) {
+                return Integer.compare(lines[o1], lines[o2]);
+            }
+        };
+        Arrays.sort(indexes, comp);
+    }
+
+    static void crash () {
+        for (int i = 0; i < size; i++) {
+            System.out.println("0");
         }
     }
 
-    static int poirot (int n) {
-        //create var count
+    static void poirot () {
+        int index = 0;
         int count = 0;
-        //iterate over n
-        for (int i = 1; i <= n; i++) {
-            //increment count if (2 to the power of i) - 1 is divisible by 7
-            if (((long) Math.pow(2, i) - 1) % 7 == 0) {
+        BigInteger num = new BigInteger("2");
+        BigInteger one = new BigInteger("1");
+        BigInteger two = new BigInteger("2");
+        BigInteger seven = new BigInteger("7");
+        BigInteger zero = new BigInteger("0");
+        int aux;
+        for (int i = 1; i <= lines[indexes[size-1]]; i++) {
+            if (i <= 0) {
+                crash();
+                System.exit(0);
+            }
+            BigInteger copy = num;
+            if (copy.subtract(one).mod(seven).compareTo(zero) == 0) {
                 count++;
             }
+            aux = indexes[index];
+            if (i == lines[aux]) {
+                cases[aux] = count;
+                index++;
+                if (index > size) {
+                    crash();
+                    System.exit(0);
+                }
+            }
+            num = num.multiply(two);
         }
-        //return count
-        return count;
     }
 
-    /*static int[] readInput(){
-        int[] lines = null;
-
-        // create buffered reader
-        BufferedReader bufferedReader = null;
-        try {
-            bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-            // read first line as int n
-            int n = Integer.parseInt(bufferedReader.readLine());
-            // create array of n ints
-            lines = new int[n];
-            // read all lines except first as ints
-            for (int i = 0; i < n; i++) {
-                lines[i] = Integer.parseInt(bufferedReader.readLine());
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+    static void printResults() {
+        for (int i = 0; i < size; i++) {
+            System.out.println(cases[i]);
         }
-        // close reader
-        try {
-            bufferedReader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return lines;
-    }*/
+    }
 
-    static int[] readInput(){
-        int[] lines = null;
+    static void readInput() {
         Scanner in = new Scanner(System.in);
-        int size = in.nextInt();
+        size = in.nextInt();
         lines = new int[size];
         for (int i = 0; i < size; i++) {
             lines[i] = in.nextInt();
         }
-
         in.close();
-
-        return lines;
     }
 
 }
